fbd9eea415ef0452b1f4a6534eae89796ac55da3
darkness <dap@open.by>
2011-09-05 23:55:29 +0300

    Possibility to work with xlsx is improved. It does not work properly =/

--- body.py
+++ body.py
@@ -20,27 +20,27 @@ def excstatistic(filename, t_sheet, t_column):
         matrix = xls(filename, t_sheet, t_column)
     else:
         text =  u"Ошибка: неправильный файл!"
-
-    if not (matrix[0]).startswith(u"Ошибка: "):
-        for i in xrange(len(matrix)):
-            if matrix[i] != "":
-                j = -1
-                try:
-                    j = name.index(matrix[i])
-                except ValueError:
-                    pass
-                if (-1 < j < len(name)):
-                    name_count[j] += 1
-                else:
-                    name.append(matrix[i])
-                    name_count.append(1)
-
-        for i in xrange(len(name)):
-            text += str(name_count[i])
-            text += u" @ "
-            text += name[i]
-            text += u"\n"
-    else:
-        text = matrix[0]
-        
+    try:
+        if not (matrix[0]).startswith(u"Ошибка: "):
+            for i in xrange(len(matrix)):
+                if matrix[i] != "":
+                    j = -1
+                    try:
+                        j = name.index(matrix[i])
+                    except ValueError:
+                        pass
+                    if (-1 < j < len(name)):
+                        name_count[j] += 1
+                    else:
+                        name.append(matrix[i])
+                        name_count.append(1)
+            for i in xrange(len(name)):
+                text += str(name_count[i])
+                text += u" @ "
+                text += name[i]
+                text += u"\n"
+        else:
+            text = matrix[0]
+    except AttributeError:
+        pass        
     return(text)
--- xlsx.py
+++ xlsx.py
@@ -5,23 +5,37 @@
 from openpyxl.reader.excel import *
 from openpyxl.workbook import *
 from openpyxl.worksheet import *
+from cPickle import dumps, loads
 
 #Загружается модуль импорта таблиц Excel *.xlsx, графическая библиотека
 #и модуль, содержащий текстовый виджет с полосой прокрутки.
 
 def xlsx(filename, t_sheet, t_column):
     matrix = []
-    try:
-        sheet_number = int(t_sheet)
-        book = load_workbook(filename)
-        sheet_names = book.get_sheet_names()
-        sheet = get_sheet_by_name(name = sheet_names[sheet_number - 1])
-        for i in xrange(sheet.get_highest_row()):
-            cell = sheet.cell(row = i, column = t_column)
-            matrix.append(cell)
-    except KeyError:
-        matrix.append(u"Ошибка: попробуйте сохранить файл в Excel 2003 - .xls!")
-    
+    if len(t_column) == 1:
+        column_number = (ord(t_column) - 65)
+        if (-1 < column_number < 26):
+             sheet_number = int(t_sheet)
+             book = load_workbook(filename)
+             sheet_names = book.get_sheet_names()
+             sheet = book.get_sheet_by_name(name = sheet_names[sheet_number - 1])
+             for i in xrange(sheet.get_highest_row()):
+                 #cell = sheet.cell(row = i, column = column_number
+                 #cell = cell.value
+                 #cell = u""
+                 cell = sheet.cell(t_column + str(i + 1))
+                 #cell = unicode(str(buffer(cell)), "utf-8")
+                 #matrix.append(str(loads(dumps(cell.value))))
+                 t = dumps(cell.value)
+                 t = t.replace("V\\", "\\")
+                 t = t.replace("\np1\n.", "")
+                 t = unicode(t, "utf-8")
+                 matrix.append(t)
+        else:
+            matrix.append(u"Ошибка: введите букву столбца от A до Z!")
+    else:
+        matrix.append(u"Ошибка: введите букву столбца от A до Z!")
+        
 
 
 #    matrix.append(u"Ошибка: обработка xlsx не готова!")
